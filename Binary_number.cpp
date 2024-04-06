a605d47248d95e3118c48a1a98037e4317ebaa1f
SAYAN-DEV-NAT <sayandevnath01@gmail.com>
2024-04-06 22:18:29 +0600

    update

--- Binary_number.cpp
+++ Binary_number.cpp
@@ -11,15 +11,16 @@ void printBinary(int a)
 int main()
 {
     int a = 5;
-    int i = 2; // kth number
+    int k = 2;
+
     printBinary(a);
 
-    ((a >> i) & 1 != 0) ? cout << "set_bit_on\n" : cout << "unset_bit_off\n";
+    ((a >> k) & 1 != 0) ? cout << "set_bit_on\n" : cout << "unset_bit_off\n";
 
-    printBinary(a | (1 << i));    // bit set -> convert
-    printBinary(a & (~(1 << i))); // bit unset -> convert
+    printBinary(a | (1 << k));    // bit set -> convert
+    printBinary(a & (~(1 << k))); // bit unset -> convert
 
-    printBinary(a ^ (1 << i)); // toggle means -> xor
+    printBinary(a ^ (1 << k)); // toggle means -> xor
     printBinary(a ^ (1 << 3)); // xor -> same==0, not_same==1
 
     return 0;
